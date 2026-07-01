87d75f7ae8029486d8fb4e63e04a0b5f09ccdc67
DovCaio <onlydeadnotalive@gmail.com>
2026-07-01 16:34:39 -0300

    feat: make the application get the variables of the system

--- product_scraper/spiders/amazon.py
+++ product_scraper/spiders/amazon.py
@@ -1,3 +1,5 @@
+import os
+
 import scrapy
 from product_scraper.items import ProductScraperItem
 
@@ -7,9 +9,7 @@ class AmazonScrapper(scrapy.Spider):
     name = "amazon"
 
 
-    start_urls = [
-        "https://www.amazon.com.br/Notebook-Gamer-Lenovo-i5-13450HX-83KHS00000/dp/B0GQJM9NJK/ref=pd_ci_mcx_mh_mcx_views_0_title?pd_rd_w=J4QPB&content-id=amzn1.sym.87e935c8-d687-4dd4-9dbf-8b9bdbb5018e%3Aamzn1.symc.c3d5766d-b606-46b8-ab07-1d9d1da0638a&pf_rd_p=87e935c8-d687-4dd4-9dbf-8b9bdbb5018e&pf_rd_r=2JC7G9E1SBYC44D54DD5&pd_rd_wg=4MHTH&pd_rd_r=ae3b70af-ac75-408e-b5b4-47c1d47f269a&pd_rd_i=B0GQJM9NJK"
-    ]
+    start_urls = os.getenv("AMAZON_PRODUCT_URLS", "").split(",")
 
 
     def parse(self, response):
